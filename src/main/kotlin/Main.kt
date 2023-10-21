e426ed584fa32106f0d18dd09681e2b73e860a84
Malox <Maxi.Junges@gmx.de>
2023-10-21 04:09:15 +0200

    do basic pruning and cleanup

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,43 @@
+import kotlin.system.measureTimeMillis
+
 fun main() {
-    simulate(
-        Madeline(0F, 0F, 345F, 0.400036633015F, 0F, 24.000047683716F, PlayerState.StClimb),
-        15,
-    )
+    println(measureTimeMillis {
+        simulate(
+            Madeline(0F, 0F, 345F, 0.400036633015F, 0F, 24.000047683716F, PlayerState.StClimb),
+            Target(0.639446F, 0.6397066F), //lambda Madeline -> Bool as success filter
+            20,
+        )
+    })
+
+    solutions.map { it }.sortedBy { it.key.ySubPixel }.forEach { (key, value) ->
+        println(key)
+        value.first.printExact()
+        println(value.second)
+        println()
+        println(value.second.toTasFile())
+    }
+
+    println(solutions.size)
+}
+
+fun List<Input>.toTasFile(): String {
+    val output = mutableListOf<String>("--- TAS starts here ---")
+
+    var counter = 1
+    var lastInput: Input = this.first()
+    for(input in this.drop(1)) {
+        if(input == lastInput) {
+            counter++
+        } else {
+            output.add("$counter, ${lastInput.TASkey}")
+            counter = 1
+            lastInput = input
+        }
+    }
+
+    output.add("$counter, ${lastInput.TASkey}")
+    output.add("--- TAS ends here ---")
+    return output.joinToString("\n")
 }
 
 fun Float.printAccurate() = println(String.format("%.12f", this))
-fun Double.printAccurate() = println(String.format("%.12f", this))
--- src/main/kotlin/Simulation.kt
+++ src/main/kotlin/Simulation.kt
@@ -1,16 +1,46 @@
-fun simulate(startMadeline: Madeline, depth: Int, path: List<Input> = emptyList()) {
-    if(depth <= 0) return
 
-    listOf(Input.None, Input.Right, Input.Grab).map { input ->
+typealias InputSequence = List<Input>
+data class YData(val yPos: Float, val ySubPixel: Float, val ySpeed: Float)
+
+val solutions: MutableMap<YData, Pair<Madeline, InputSequence>> = mutableMapOf()
+val cache: HashSet<YData> = hashSetOf()
+fun simulate(startMadeline: Madeline, target: Target, depth: Int, path: List<Input> = emptyList()) {
+    if(depth < 0) return
+    val key = YData(startMadeline.y, startMadeline.yMovementCounter, startMadeline.ySpeed)
+    if(cache.contains(key)) {
+        return
+    } else {
+        cache.add(key)
+        if(startMadeline.yMovementCounter <= target.upperBound && startMadeline.yMovementCounter >= target.lowerBound) {
+            solutions[key] = startMadeline to path
+        }
+    }
+
+    val inputs = when(startMadeline.state) {
+        PlayerState.StClimb -> {
+            if(startMadeline.ySpeed < 15) {
+                listOf(Input.None)
+            } else {
+                listOf(Input.None, Input.Grab)
+            }
+        }
+        else -> listOf(Input.None, Input.Right, Input.Grab)
+    }
+
+    inputs.map { input ->
         val newMadeline = startMadeline.copy()
         newMadeline.update(input)
         val newPath = path + listOf(input)
+        simulate(newMadeline, target, depth - 1, newPath)
+    }
+}
 
-        if(newMadeline.yMovementCounter < 0.6397066F - 1F && newMadeline.yMovementCounter > 0.639446F - 1F) {
-            newMadeline.printExact()
-            println(newPath)
-            return
-        }
-        simulate(newMadeline, depth - 1, newPath)
+class Target(lowerBoundParam: Float, upperBoundParam: Float) {
+    val upperBound: Float
+    val lowerBound: Float
+
+    init {
+        upperBound = if(upperBoundParam > 0.5F) upperBoundParam - 1F else upperBoundParam
+        lowerBound = if(lowerBoundParam > 0.5F) lowerBoundParam - 1F else lowerBoundParam
     }
 }
